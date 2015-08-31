13381c00623ad5bd7f8f2dc6e7958ef28f25b9b0
ARollingScone <jnfreeston@gmail.com>
2015-08-31 01:13:18 +0100

    Testing Serialization

---
+++ BleedinVampires/Control/GameState.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+using BleedinVampires.Entities;
+
+
+namespace BleedinVampires.Control
+{
+    //This class represents the state of the game
+    //This is what the server sends to the client
+
+    [Serializable]
+    class GameState
+    {
+        //Contains a list of each type etc
+        List<PlayerEntity> list_Players;
+
+        //List of Enemies
+
+        //List of Dynamic Blocks
+    }
+
+    //De/Serializes the game state into a byte[] using Binary Formatter
+    static class GameStateSerializer
+    {
+        public static byte[] ToPacket(GameState GS)
+        {
+            byte[] packet;
+            BinaryFormatter serializer = new BinaryFormatter();
+
+            using (var ms = new MemoryStream())
+            {
+                using (var ds = new DeflateStream(ms, CompressionMode.Compress, true))
+                {
+                    serializer.Serialize(ds, GS);
+                }
+                packet = ms.ToArray();
+            }
+            return packet;
+        }
+
+        public static GameState FromPacket(byte[] packet)
+        {
+            GameState GS;
+            BinaryFormatter serializer = new BinaryFormatter();
+
+            using (var ms = new MemoryStream(packet))
+            {
+                using (var ds = new DeflateStream(ms, CompressionMode.Decompress, true))
+                {
+                    GS = (GameState)serializer.Deserialize(ds);
+                }
+            }
+            return GS;
+        }  
+    }
+}
---
+++ BleedinVampires/Entities/TestSerialize.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.System;
+
+namespace BleedinVampires.Entities
+{
+    [Serializable]
+    class TestSerialize
+    {
+        public int id;
+        public string msg;
+
+        public float x;
+        public float y;
+
+        public TestSerialize(int i)
+        {
+            Random r = new Random(i);
+            id = r.Next(999);
+
+            x = (float) (10 + r.Next(20)) / (1 + r.Next(10));
+            y = (float)(10 + r.Next(20)) / (1 + r.Next(10));
+
+            msg = "Hello World";
+
+            string data = "T," + id + ",M," + msg + ",X," + x + ",Y," + y;
+
+            //rotation = (float)(0 + r.Next(360)) / (1 + r.Next(10));
+
+            //Console.WriteLine("My data size is: " + (System.Text.ASCIIEncoding.Unicode.GetByteCount(data).ToString()));
+        }
+
+    }
+}
